6e3da8278f7cc5db1e35f64b5bcaa7fe87a5bd4f
mdubelbeis <mason.dubelbeis@gmail.com>
2022-06-09 21:26:24 -0400

    created for social svgs

---
+++ src/components/footer/SocialLinks.jsx
@@ -0,0 +1,20 @@
+const SocialLinks = ({
+  href = '#',
+  width = '20',
+  height = '20',
+  dPath,
+  fillColor = '#2C7566',
+  fillRule = 'nonzero',
+}) => {
+  return (
+    <div>
+      <a href={href}>
+        <svg width={width} height={height}>
+          <path d={dPath} fill={fillColor} fillRule={fillRule} />
+        </svg>
+      </a>{' '}
+    </div>
+  );
+};
+
+export default SocialLinks;
