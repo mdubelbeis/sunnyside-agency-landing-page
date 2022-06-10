9368f6e3deb6eea375992d3cfe5ee55ccb581a45
mdubelbeis <mason.dubelbeis@gmail.com>
2022-06-10 00:24:56 -0400

    added height

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,17 +8,17 @@ const Header = () => {
     windowWidth < 768 ? 'bg-mobile-header-image' : 'bg-desktop-header-image';
   return (
     <header
-      className={`w-full h-[540px] ${headerImg} bg-no-repeat bg-cover bg-center object-contain p-4`}
+      className={`w-full h-[750px] ${headerImg} bg-no-repeat bg-cover bg-center object-contain p-4`}
     >
       <Navbar />
       <div className="flex flex-col items-center space-y-16">
         <div className="mt-16">
-          <h1 className="font-fraunces text-pure-white font-bold text-4xl px-4 text-center tracking-widest md:text-7xl">
+          <h1 className="font-fraunces text-pure-white font-extrabold text-4xl px-4 text-center tracking-widest md:text-7xl">
             WE ARE CREATIVES
           </h1>
         </div>
         <div>
-          <svg width="36" height="114" xmlns="http://www.w3.org/2000/svg">
+          <svg width="36" height="125" xmlns="http://www.w3.org/2000/svg">
             <g
               stroke="#FFF"
               strokeWidth="6"
