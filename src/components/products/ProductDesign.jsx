dfba59efa03ebca083dc782fc9e59797e470d251
mdubelbeis <mason.dubelbeis@gmail.com>
2022-06-09 23:28:34 -0400

    corrected height and responsiveness

--- src/components/products/ProductDesign.jsx
+++ src/components/products/ProductDesign.jsx
@@ -1,9 +1,9 @@
 const ProductDesign = ({ title, description, bgImage }) => {
   return (
     <div
-      className={`${bgImage} bg-no-repeat bg-center bg-fit object-fit w-full h-[600px] flex justify-center items-end font-fraunces`}
+      className={`${bgImage} bg-no-repeat bg-top bg-cover object-fit md:w-6/12 h-[650px] lg:h-[800px] flex justify-center items-end font-fraunces`}
     >
-      <div className="space-y-8 p-4 text-center mb-10 opacity-75 sm:px-28 md:px-18 lg:px-32">
+      <div className="space-y-4 p-10 text-center opacity-75 md:px-18 lg:px-32">
         <h2 className="text-3xl text-very-dark-desaturated-blue">{title}</h2>
         <p className="text-md font-barlow leading-6 tracking-wide text-dark-blue font-light">
           {description}
