2e050eae8c762500c73b4fae2190858019171325
mdubelbeis <mason.dubelbeis@gmail.com>
2022-06-09 23:56:14 -0400

    added short term responsive image fix

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,28 +1,36 @@
-import mobileMilkBottles from '../../assets/mobile/image-gallery-milkbottles.jpg';
-import mobileCone from '../../assets/mobile/image-gallery-cone.jpg';
-import mobileOrange from '../../assets/mobile/image-gallery-orange.jpg';
-import mobileSugarCubes from '../../assets/mobile/image-gallery-sugar-cubes.jpg';
+import { useState } from 'react';
+
+import mobileMilk from '/assets/mobile/image-gallery-milkbottles.jpg';
+import mobileOrange from '/assets/mobile/image-gallery-orange.jpg';
+import mobileCone from '/assets/mobile/image-gallery-cone.jpg';
+import mobileCubes from '/assets/mobile/image-gallery-sugar-cubes.jpg';
 
 // Still going to have to figure out best way to use srcSet for responsiveness
-import desktopMilkBottles from '../../assets/desktop/image-gallery-milkbottles.jpg';
-import desktopCone from '../../assets/desktop/image-gallery-cone.jpg';
-import desktopOrange from '../../assets/desktop/image-gallery-orange.jpg';
-import desktopSugarCubes from '../../assets/desktop/image-gallery-sugarcubes.jpg';
+import desktopMilk from '/assets/desktop/image-gallery-milkbottles.jpg';
+import desktopCone from '/assets/desktop/image-gallery-cone.jpg';
+import desktopOrange from '/assets/desktop/image-gallery-orange.jpg';
+import desktopCubes from '/assets/desktop/image-gallery-sugarcubes.jpg';
 
 const Images = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const milk = windowWidth < 768 ? mobileMilk : desktopMilk;
+  const orange = windowWidth < 768 ? mobileOrange : desktopOrange;
+  const cone = windowWidth < 768 ? mobileCone : desktopCone;
+  const cubes = windowWidth < 768 ? mobileCubes : desktopCubes;
   return (
     <div className="grid grid-cols-2 mt-6 md:grid-cols-4">
       <div>
-        <img src={mobileMilkBottles} alt="milk bottles" />{' '}
+        <img src={milk} alt="milk bottles" />{' '}
       </div>
       <div>
-        <img src={mobileOrange} alt="orange on plate" />{' '}
+        <img src={orange} alt="orange on plate" />{' '}
       </div>
       <div>
-        <img src={mobileCone} alt="ice cream cone" />{' '}
+        <img src={cone} alt="ice cream cone" />{' '}
       </div>
       <div>
-        <img src={mobileSugarCubes} alt="sugar cubes" />
+        <img src={cubes} alt="sugar cubes" />
       </div>
     </div>
   );
